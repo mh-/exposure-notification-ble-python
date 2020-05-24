1443d866f74bdad3c91fb0e0559afefd16d14e35
Michael Huebler <michael@huebler.org>
2020-05-24 17:21:48 +0200

    Also store base64-encoded version of TEK in tek_data.csv

--- lib/en_tx_data_store.py
+++ lib/en_tx_data_store.py
@@ -1,11 +1,14 @@
+import base64
+
+
 class ENTxDataStore:
     def __init__(self, tx_raw_data_file_name):
         self.tek_data_file = open(tx_raw_data_file_name, 'a')
-        self.tek_data_file.write("TEK_Roll_Interval_i;TEK\n")
+        self.tek_data_file.write("TEK_Roll_Interval_i;TEK;TEK-base64\n")
 
     def __del__(self):
         self.tek_data_file.close()
 
     def write(self, i, tek):
-        self.tek_data_file.write("%d;%s\n" % (i, tek.hex()))
+        self.tek_data_file.write("%d;%s;%s\n" % (i, tek.hex(), base64.b64encode(tek).decode(encoding="UTF-8")))
         self.tek_data_file.flush()
